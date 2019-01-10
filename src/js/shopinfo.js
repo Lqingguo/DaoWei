c9f671fbc408eb6d43a779c40c9d41a7e49e4df8
liqing <www.liqingguo1999@qq.com>
2019-01-10 14:37:16 +0800

    分页完成，项目结束

--- src/js/shopinfo.js
+++ src/js/shopinfo.js
@@ -15,7 +15,7 @@ $(function(){
 
       $.getJSON('./mock/comment.json',function(data){
         $(data).each(function(index,item){
-          $('.navx').append(` <div class="commen">
+          $('.navx').append(`
       <ul class="userlist">
         <li>
           <img src="${item.iconUrl}" alt="">
@@ -35,7 +35,100 @@ $(function(){
         </li>
       </ul>
 
-    </div>`)
+    `)
         })
+
+        tabPage({
+          pageNav: '#pageNav',
+          pagePrev: '#prev',
+          pageNext: '#next',
+          curNum: 10, /*每页显示的条数*/
+          activeClass: 'active', /*高亮显示的class*/
+          ini: 0/*初始化显示的页面*/
+        })
+        function tabPage(tabPage){
+          var pageNav = $(tabPage.pageNav);
+          /*获取分页*/
+          var pagePrev = $(tabPage.pagePrev);
+          /*上一页*/
+          var pageNext = $(tabPage.pageNext);
+          /*下一页*/
+          var iNum = 0
+          //初始下标
+          var curNum = tabPage.curNum
+          //每页显示的个数
+          var len = Math.ceil(data.length / curNum);
+          console.log(len)
+          //生成页码
+          for(var i = 0;i<len;i++){
+            $('#pageNav').append(`<a href="javascript:;">${i+1}</a>`)
+          }
+          pageNav.find("a:first").addClass(tabPage.activeClass);
+          //标签页的点击事件
+          pageNav.find('a').each( function(){
+            $(this).click(function(){
+              $(this).addClass('active').siblings().removeClass('active')
+              //让当前索引等于点击的索引
+              iNum = $(this).index();
+              console.log(iNum)
+              $('.navx>.userlist').hide();
+              for (var i = ($(this).html() - 1) * curNum; i < ($(this).html()) * curNum; i++) {
+                $('.navx>.userlist').eq(i).show()
+              }
+
+            })
+          })
+          //让其余的li标签隐藏   ++++++++===============
+          $('.navx>.userlist').hide();
+          /************首页的显示*********/
+          for (var i = 0; i < curNum; i++) {
+            $('.navx>.userlist').eq(i).show()
+          }
+          //点击下一页
+          pageNext.click(function(){
+            //所有的隐藏
+            $('.navx>.userlist').hide()
+            if(iNum === len-1){
+              alert('这已经是最后的一页了')
+
+              //显示最后一页内容
+              for(var i=(len-1)*curNum;i<len*curNum;i++){
+                $('.navx>.userlist').eq(i).show()
+              }
+              return false
+            }else {
+              pageNav.find("a").removeClass(tabPage.activeClass);
+              iNum++;
+              pageNav.find("a").eq(iNum).addClass(tabPage.activeClass);
+
+              for(var i=iNum*curNum;i<(iNum+1)*curNum;i++){
+                $('.navx>.userlist').eq(i).show()
+              }
+
+            }
+            //
+          })
+          //点击下一页
+          pagePrev.click(function(){
+            $('.navx>.userlist').hide()
+              if(iNum === 0){
+                  alert('这已经是前边的一页了')
+                //显示第一页内容
+                for(var i=0;i<curNum;i++){
+                  $('.navx>.userlist').eq(i).show()
+                }
+                return false
+              }else {
+                pageNav.find("a").removeClass(tabPage.activeClass);
+                iNum--;
+                ageNav.find("a").eq(iNum).addClass(tabPage.activeClass);
+              }
+              for(var i = iNum*curNum;i<(iNum+1)*curNum;i++){
+                $('.navx>.userlist').eq(i).show()
+              }
+          })
+        }
       })
+
+
   })
